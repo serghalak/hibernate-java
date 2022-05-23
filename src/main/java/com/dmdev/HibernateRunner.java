c4203819d8266e9f92927f4c733904c2315ea571
serghalak <serghalak@gmail.com>
2022-05-23 15:15:50 +0300

    add persistence.xml for create EntityManagerFactory

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -1,19 +1,36 @@
 package com.dmdev;
 
+import com.dmdev.model.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class HibernateRunner {
 
     public static void main(String[] args) {
         System.out.println("Hello hibernate!!!");
-        Configuration configuration = new Configuration();
-        configuration.configure("hibernate.cfg.xml");
-
-        try ( SessionFactory sessionFactory = configuration.buildSessionFactory();
-              Session session = sessionFactory.openSession()) {
-            System.out.println("Session (as a wrapper about connection in jdbc) is created");
-        }
+//        Configuration configuration = new Configuration();
+//        configuration.configure("hibernate.cfg.xml");
+//
+//        try ( SessionFactory sessionFactory = configuration.buildSessionFactory();
+//              Session session = sessionFactory.openSession()) {
+//            System.out.println("Session (as a wrapper about connection in jdbc) is created");
+//        }
+        useEntityManagerWithHibernate();
     }
+
+    private static void useEntityManagerWithHibernate() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        EntityManager em=emf.createEntityManager();
+        em.getTransaction().begin();
+        final Student student = em.find(Student.class, 101L);
+        System.out.println(student);
+        em.getTransaction().commit();
+    }
+
+
 }
---
+++ src/main/java/com/dmdev/model/Student.java
@@ -0,0 +1,84 @@
+package com.dmdev.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Student {
+
+    @Id
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student user = (Student) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
